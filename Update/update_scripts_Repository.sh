8e4ad5ba774054804a97f0d76e2db202724f5783
SebastianHanz <sebastianhanz@gmx.de>
2021-02-04 19:25:33 +0100

    Update update_scripts_Repository.sh

--- Update/update_scripts_Repository.sh
+++ Update/update_scripts_Repository.sh
@@ -10,10 +10,12 @@ DOWNLOADS=/tmp/Github-Download
 mkdir $DOWNLOADS
 cd $DOWNLOADS
 
+echo Lade aktuelle GitHub-Repo herunter
 git clone https://0b6d26137c4026ddd46f5356e8329e8989b80ecd:x-oauth-basic@github.com/SebastianHanz/Rock64NAS-Scripts ./
 
-chmod +x ./*
-
+chmod -R +x ./*
+rm ./Update/update_scripts_Repository.sh
 cp -r ./* /$SRC_SCRIPTS
-
 rm -r $DOWNLOADS
+
+echo Finish!
