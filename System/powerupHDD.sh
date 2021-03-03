cd718416cee54e6bfd4bd4116e87d086309223a0
SebastianHanz <sebastianhanz@gmx.de>
2021-03-03 18:24:07 +0100

    added delays and little cleanup
    added waittimes especially for mounting filesystem before starting Docker-container

--- System/powerupHDD.sh
+++ System/powerupHDD.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# MOUNTING FILESYSTEM
 # Mounting HDD/Raid-System
 echo -e "Mounting HDD/Raid.."
 mount -a
-echo -e "Mounted."
+echo -e "Mounted. Wait for system to get ready..."
 
+sleep 30 # Wait for system to get ready
+
+# SERVICES
 # Starting all OMV-Services which operate with or on HDDs
 echo -e "Starting system-services..."
 service minidlna start
@@ -12,12 +16,20 @@ service proftpd start
 service smbd start
 echo -e "All necessary services started! \n"
 
+# DOCKER
 # Starting all necessary Docker-Containers
 echo -e "Starting Docker-Containers"
-docker start NextcloudDB
-docker start Nextcloud
-docker start Nextcloud_TestEnv
+docker start NextcloudDB # Nextcloud Database
+sleep 10                 # Wait for DB-Ready
+docker start Nextcloud   # Nextcloud
+
+# Optional Test-Environment
 docker start NextcloudDB_TestEnv
+sleep 10
+
+docker start Nextcloud_TestEnv
 echo -e "All necessary containers started! \n"
+
+# END
 echo -e "Finish!"
 exit
